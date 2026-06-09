36ee929e60935c20a00f3b10f42c1b255789a47c
AtalaaDf <mataladaffaalfaristi@gmail.com>
2026-06-09 09:13:00 +0700

    feat: connect all providers to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {ThemeProvider} from "./context/ThemeContext.jsx"
+import {SaveBookProvider} from "./context/SavedBooksContext.jsx"
+import {SearchProvider} from "./context/SearchContext.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <SaveBookProvider>
+        <SearchProvider>
+          <App />
+        </SearchProvider>
+      </SaveBookProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
