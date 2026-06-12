b0adec01c4ba79af1a292c63ab706ffbbce41adc
AtalaaDf <mataladaffaalfaristi@gmail.com>
2026-06-12 09:38:10 +0700

    feat: add popular page and fetchPopularBooks

--- src/api/bookApi.js
+++ src/api/bookApi.js
@@ -13,6 +13,19 @@ export async function fetchTrendingBooks(page = 1) {
     }
 }
 
+export async function fetchPopularBooks(page = 1) {
+    try {
+        const response = await fetch(`${BASE_URL}/trending/weekly.json?page=${page}&limit=12`)
+        if (!response.ok) {
+        throw new Error("gagal mengambil data")
+        }
+        const data = await response.json()
+        return data
+    } catch (error) {
+        throw error
+    }
+}
+
 export async function searchBooks(query) {
     try {
         const response = await fetch(`${BASE_URL}/search.json?q=${query}`)
--- src/pages/PopularPage.jsx
+++ src/pages/PopularPage.jsx
@@ -1,8 +1,67 @@
+import { useState, useEffect } from "react"
+import {Loading} from"../components/Loading"
+import {fetchPopularBooks} from"../api/bookApi"
+import ErrorPage from"../pages/ErrorPage"
+import BookCard from "../components/BookCard"
+import { ArrowLeft, ArrowRight } from "lucide-react"
+
 function PopularPage(){
-    return(
-        <>
-        <h1>Popular page</h1>
-        </>
+        const [books, setBooks] = useState([])
+        const [loading, setLoading] = useState(true)
+        const [error, setError] = useState(null)
+        const [currentPage, setCurrentPage] = useState(1)
+        const [totalPage, setTotalPage] = useState(0)
+
+    useEffect(() => {
+    const loadData = async () => {
+        try {
+            setLoading(true)
+            const data = await fetchPopularBooks(currentPage)
+            setBooks(data.works)
+            setTotalPage(Math.ceil(data.numFound / 12))
+        } catch (error) {
+            setError(error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+    
+    loadData()
+    }, [currentPage])
+
+    if (loading) return <Loading />
+
+    if (error) return <ErrorPage message={error} />
+
+    return( 
+    <div>
+        <h2>Popular Books</h2>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {books.map((book)=>(
+                <BookCard key={book.key} book={book}/>
+            ))}
+        </div>
+
+            <div className="join">
+                <button onClick={() => setCurrentPage(currentPage -1)} disabled={currentPage === 1}>
+                    <ArrowLeft/>
+                </button>
+
+                {Array.from({length: totalPage}, (_, i) => i +1)
+                .map((page)=>(
+                <button key={page} className={`join-item btn ${page === currentPage ? "btn-active" : ""}`}
+                onClick={() => setCurrentPage(page)}>
+                    {page}
+                </button>
+                ))}
+                
+                <button onClick={()=>setCurrentPage(currentPage +1)} disabled={currentPage === totalPage}>
+                    <ArrowRight/>
+                </button>
+            </div>
+
+    </div>
     )
 }
 
