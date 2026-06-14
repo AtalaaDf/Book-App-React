801abbe58ed8c241b2afab07c7cf8fddf3754df3
AtalaaDf <mataladaffaalfaristi@gmail.com>
2026-06-14 09:25:48 +0700

    feat: add synopsis components

--- src/components/Detail/BookSynopsis.jsx
+++ src/components/Detail/BookSynopsis.jsx
@@ -1,6 +1,19 @@
 export function BookSynopsis({book}){
+
+    const description = typeof book.description === 'string' ? 
+    book.description : 
+    book.description?.value
+
     return(
-        <>
-        </>
+        <div>
+            <h2>
+                Sinopsis
+            </h2>
+
+            <div>
+                {description || "sinopsis tidak tersedia"}
+            </div>
+
+        </div>
     )
 }
