79884702a8ef0fbaf2a1a7bc73402600584b6b1e
AtalaaDf <mataladaffaalfaristi@gmail.com>
2026-06-16 09:36:10 +0700

    feat: add BookMeta components

--- src/components/Detail/BookMeta.jsx
+++ src/components/Detail/BookMeta.jsx
@@ -1,6 +1,8 @@
 export function BookMeta({book}){
     return(
-        <>
-        </>
+    <div className="grid grid-cols-2 gap-4">
+        <p>Tahun terbit: <span>{book.first_publish_date}</span></p>
+        <p>Bahasa: <span>{book.languages?.[0]?.key.replace('/languages/', '')}</span></p>
+    </div>
     )
 }
