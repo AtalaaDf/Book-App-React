2267cf211648b3a86c3c012c86ebbcda027dde4f
AtalaaDf <mataladaffaalfaristi@gmail.com>
2026-06-17 12:56:03 +0700

    feat: add CarouselSection and PopupCard

--- src/components/Home/CarouselSection.jsx
+++ src/components/Home/CarouselSection.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react"
+import {Loading} from '../../components/Loading'
+import {fetchTrendingBooks} from '../../api/bookApi'
+import {PopupCard} from './PopupCard'
+import ErrorPage from '../../pages/ErrorPage'
+
 export function CarouselSection(){
+        const [books, setBooks] = useState([])
+        const [loading, setLoading] = useState(true)
+        const [error, setError] = useState(null)
+        const [selectedBook, setSelectedBook] = useState(null)
+
+        useEffect(() => {
+            const loadData = async () => {
+                try {
+                    setLoading(true)
+                    const data = await fetchTrendingBooks()
+                    setBooks(data.works)
+                } catch (error) {
+                    setError(error.message)
+                } finally {
+                    setLoading(false)
+                }
+            }
+            
+            loadData()
+            }, [])
+
+            if (loading) return <Loading />
+
+            if (error) return <ErrorPage message={error} />
+
     return(
-        <>
-        Carousel Section
-        </>
+        <div>
+        <h2>Judul Section</h2>
+        <div className="flex gap-4 overflow-x-auto">
+            {books.map((book) => (
+                <div 
+                    key={book.key}
+                    onClick={() => setSelectedBook(book)}
+                >
+                    <p>{book.title}</p>
+                </div>
+            ))}
+        </div>
+
+        {selectedBook && <PopupCard book={selectedBook} onClose={() => setSelectedBook(null)}/>}
+    </div>
     )
 }
--- src/components/Home/PopupCard.jsx
+++ src/components/Home/PopupCard.jsx
@@ -1,7 +1,21 @@
-export function Popupcard(){
+import { useNavigate } from "react-router-dom"
+import { X } from "lucide-react"
+
+export function PopupCard({book, onClose}){
+    const navigate = useNavigate()
+
     return(
-        <>
-        Popup Card
-        </>
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center">
+            <h3>{book.title}</h3>
+            <p>
+                {book.description}
+            </p>
+            <button onClick={onClose}>
+                <X/>
+            </button>
+            <button onClick={()=>  navigate(`/detail/${book.key.replace('/works/', '')}`)}>
+                Selengkapnya
+            </button>
+        </div>
     )
 }
