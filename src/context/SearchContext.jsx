cebaeca5e422c516b08b7f1ae50ec9532ae8af7a
AtalaaDf <mataladaffaalfaristi@gmail.com>
2026-06-22 13:37:56 +0700

    feat: add keyword state to SearchContext

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -3,13 +3,14 @@ const SearchContext = createContext()
 
 export function SearchProvider({children}){
     const [isSearchOpen, setIsSearchOpen] = useState(false)
+    const [keyword, setKeyword] = useState("")
 
     const toggleSearch = () => {
         setIsSearchOpen(!isSearchOpen)
     }
 
     return(
-        <SearchContext.Provider value={{isSearchOpen, toggleSearch}}>
+        <SearchContext.Provider value={{isSearchOpen, toggleSearch, keyword, setKeyword}}>
             {children}
         </SearchContext.Provider>
     )
