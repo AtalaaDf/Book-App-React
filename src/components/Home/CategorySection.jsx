16c1c5d7f662ca38fb7cb415538b6e00cdf4a9f0
AtalaDf <mataladaffaalfaristi@gmail.com>
2026-06-25 10:37:32 +0700

    feat: add CategorySection

--- src/components/Home/CategorySection.jsx
+++ src/components/Home/CategorySection.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
+    const arr = ["Romance", "Fiction", "Non-Fiction", "Mistery",
+                "Science-Ficton", "fantasy", "Horror", "History", "Advanture"
+     ]
+
 export function CategorySection(){
+    const navigate = useNavigate()
+
     return(
-        <>
-        category Section
-        </>
+        <div>
+        <h1>You can see what Genre u want!</h1>
+        {arr.map((genre)=>(
+            <button key={genre} onClick={()=>navigate(`/search/${genre}`)}>{genre}</button>
+        ))}
+        </div>
     )
 }
