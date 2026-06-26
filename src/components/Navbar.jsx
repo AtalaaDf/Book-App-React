942badf35042a6bec07744f22e55167fcbe3bb38
AtalaDf <mataladaffaalfaristi@gmail.com>
2026-06-26 11:52:43 +0700

    feat: add navbar style(not finished yet)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,34 @@ import {useTheme} from "../context/ThemeContext.jsx"
 import {useSavedBooks} from "../context/SavedBooksContext.jsx"
 import {useSearch} from "../context/SearchContext.jsx"
 import { Search, Moon, Sun, Menu, Bookmark } from "lucide-react"
+import { useState, useEffect } from "react"
 
 function Navbar(){
     const { isDark, toggleTheme } = useTheme()
     const { savedBooks } = useSavedBooks()
     const { toggleSearch } = useSearch()
+    const [scroll, setScroll] = useState(0)
+    const progress = Math.min(scroll / 200, 1)
+    const opacity = 1 - (progress * 0.7)
+    const width = 100 - (progress * 70)
+
+    useEffect(() => {
+    function handleScroll() {
+       setScroll(window.scrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll)
+    }
+}, [])
+
     return(
-        <nav className="navbar bg-base-100 shadow-sm">
+        <nav 
+            className="navbar sticky bg-base-100 shadow-sm fixed top-0 left-0 right-0 transition-all duration-300 z-10"
+            style={{ opacity: opacity, width: `${width}%`, margin: '0 auto' }}
+        >
 
             <div className="navbar-start">
                 <NavLink 
